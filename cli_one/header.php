bfd778a136c4142227a5c5e6902e668d9e60fb3d
Rishabh Nahar <rishabhnahar1202@gmail.com>
2023-01-24 16:27:11 +0530

    Search and nav fix

--- cli_one/header.php
+++ cli_one/header.php
@@ -350,20 +350,41 @@
                             </ul>
                         </nav>
                     </section>
+                    <!-- <div class="search-container">
+                        <div class="search-box">
+                            <input type="text" name="" id="">
+                            <i class="fa fa-search"></i>
+                        </div>
+                    </div> -->
                 </div>
-                <div class="velaHeaderRight col-xs-7 col-sm-6 col-md-3">
 
-                    <div class="velaCartTop">
+
+                <div class="velaHeaderRight col-xs-7 col-sm-6 col-md-3">
+                    <div class="header-search-icon" onclick="openSearchModal()">
+                        <!-- <i class="fa fa-search icons"></i> -->
+                        <i class="icons">
+                            <svg width="20px" height="20px" viewBox="0 0 32 32" style="enable-background: new 0 0 512 512" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                                <g id="icomoon-ignore">
+                                </g>
+                                <path d="M28.591 27.273l-7.263-7.264c1.46-1.756 2.339-4.010 2.339-6.471 0-5.595-4.535-10.129-10.129-10.129-5.594 0-10.129 4.535-10.129 10.129 0 5.594 4.536 10.129 10.129 10.129 2.462 0 4.716-0.879 6.471-2.339l7.263 7.264 1.319-1.319zM4.475 13.538c0-4.997 4.065-9.063 9.063-9.063 4.997 0 9.063 4.066 9.063 9.063s-4.066 9.063-9.063 9.063c-4.998 0-9.063-4.066-9.063-9.063z" />                                
+                            </svg>
+                        </i>
+                    </div>
+                    <div class="velaCartTop" style="white-space: nowrap;">
                         <!-- <a href="cart.php" class="jsDrawerOpenRight d-flex"> -->
                         <a href="cart.php">
-                            <i class="icons"><svg version="1.1" xmlns="http://www.w3.org/2000/svg"
+                            <i class="icons">
+                                <svg version="1.1" xmlns="http://www.w3.org/2000/svg"
                                     xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512"
                                     style="enable-background: new 0 0 512 512" xml:space="preserve">
                                     <path
                                         d="M448,160h-64v-4.5C384,87,329,32,260.5,32h-8C184,32,128,87,128,155.5v4.5H64L32,480h448L448,160z M160,155.5c0-50.7,41.8-91.5,92.5-91.5h7.5h0.5c50.7,0,91.5,40.8,91.5,91.5v4.5H160V155.5z M67.8,448l24.9-256H128v36.3c-9.6,5.5-16,15.9-16,27.7c0,17.7,14.3,32,32,32s32-14.3,32-32c0-11.8-6.4-22.2-16-27.7V192h192v36.3c-9.6,5.5-16,15.9-16,27.7c0,17.7,14.3,32,32,32s32-14.3,32-32c0-11.8-6.4-22.2-16-27.7V192h35.4l24.9,256H67.8z" />
-                                </svg></i>
+                                </svg>
+                            </i>
                             <span class="cartitle hidden-xs"> Cart</span>
-                            (<span id="CartCount"> 0 </span>)
+                            <span class="cart-count-container">
+                                (<span id="CartCount"> 0 </span>)
+                            </span>
                         </a>
                     </div>
                     <div class="box"
@@ -394,3 +415,234 @@
         </div>
     </div>
 </section>
+
+<div id="velaMenuMobile" class="menuMobileContainer hidden-md hidden-lg">
+    <div class="menuMobileWrapper">
+        <div class="memoHeader">
+            <span>AToZ</span>
+            <div class="btnMenuClose">&nbsp;</div>
+        </div>
+        <ul class="nav memoNav">
+            <li class="active">
+                <a class="collapsed" href="/" title="" data-toggle="collapse"
+                    data-target="#memoDropdown11">Home</a>
+            </li>
+            <li class="hasMemoDropdown">
+                <a class="collapsed" href="/collections/all" title="" data-toggle="collapse"
+                    data-target="#memoDropdown12">Shop</a>
+                <span class="memoBtnDropdown collapsed" data-toggle="collapse"
+                    data-target="#memoDropdown12"><i class="fa fa-angle-down"></i></span>
+                <ul id="memoDropdown12" class="memoDropdown collapse">
+                    <li class="hasMemoDropdown">
+                        <a class="collapsed" href="#/collections" title="" data-toggle="collapse"
+                            data-target="#memoDropdown221">Catalog</a>
+                        <span class="memoBtnDropdown collapsed" data-toggle="collapse"
+                            data-target="#memoDropdown221"><i class="fa fa-angle-down"></i></span>
+                        <ul id="memoDropdown221" class="memoDropdown collapse">
+                            <li class="">
+                                <a href="collections/furniture.php" title="">Furniture</a>
+                            </li>
+                            <li class="">
+                                <a href="collections/decor-art.php" title="">Decor Art</a>
+                            </li>
+                            <li class="">
+                                <a href="collections/illumination.php" title="">Illumination</a>
+                            </li>
+                            <li class="">
+                                <a href="collections/kitchen-things.php" title="">Kitchen Things</a>
+                            </li>
+                            <li class="">
+                                <a href="collections/best-sellter.php" title="">Best Sellter</a>
+                            </li>
+                            <li class="">
+                                <a href="collections/new-products.php" title="">New products</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="hasMemoDropdown">
+                        <a class="collapsed" href="#/collections/best-sellter" title=""
+                            data-toggle="collapse" data-target="#memoDropdown222">Shop pages</a>
+                        <span class="memoBtnDropdown collapsed" data-toggle="collapse"
+                            data-target="#memoDropdown222"><i class="fa fa-angle-down"></i></span>
+                        <ul id="memoDropdown222" class="memoDropdown collapse">
+                            <li class="">
+                                <a href="collections/sale-off.php" title="">Left Sidebar</a>
+                            </li>
+                            <li class="">
+                                <a href="collections/best-sellter.php" title="">Right Sidebar</a>
+                            </li>
+                            <li class="">
+                                <a href="collections/furniture.php" title="">Collection List</a>
+                            </li>
+                            <li class="">
+                                <a href="collections/all.php" title="">Collection Grid</a>
+                            </li>
+                            <li class="">
+                                <a href="collections/kitchen-things.php" title="">Full Width</a>
+                            </li>
+                            <li class="">
+                                <a href="collections/illumination.php" title="">Full Width V1</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="hasMemoDropdown">
+                        <a class="collapsed" href="#/products/sacrificial-chair-design-12" title=""
+                            data-toggle="collapse" data-target="#memoDropdown223">Product Pages</a>
+                        <span class="memoBtnDropdown collapsed" data-toggle="collapse"
+                            data-target="#memoDropdown223"><i class="fa fa-angle-down"></i></span>
+                        <ul id="memoDropdown223" class="memoDropdown collapse">
+                            <li class="">
+                                <a href="products/sacrificial-chair-design-12.php" title="">Product Page
+                                    V1</a>
+                            </li>
+                            <li class="">
+                                <a href="products/sacrificial-chair-design-5.php" title="">Product Page
+                                    V2</a>
+                            </li>
+                            <li class="">
+                                <a href="products/sacrificial-chair-design-6.php" title="">Product Page
+                                    V3</a>
+                            </li>
+                            <li class="">
+                                <a href="products/sacrificial-chair-design-10.php" title="">Product Page
+                                    V4</a>
+                            </li>
+                            <li class="">
+                                <a href="products/sacrificial-chair-design-11.php" title="">Product Page
+                                    V5</a>
+                            </li>
+                            <li class="">
+                                <a href="products/sacrificial-chair-design-2.php" title="">Variable
+                                    Product</a>
+                            </li>
+                            <li class="">
+                                <a href="products/sacrificial-chair-design-8.php" title="">External
+                                    Product</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="hasMemoDropdown">
+                        <a class="collapsed" href="#/pages/home-pages" title="" data-toggle="collapse"
+                            data-target="#memoDropdown224">Other Pages</a>
+                        <span class="memoBtnDropdown collapsed" data-toggle="collapse"
+                            data-target="#memoDropdown224"><i class="fa fa-angle-down"></i></span>
+                        <ul id="memoDropdown224" class="memoDropdown collapse">
+                            <li class="">
+                                <a href="about-us.php" title="">About Page</a>
+                            </li>
+                            <li class="">
+                                <a href="contact-us.php" title="">Contact Page</a>
+                            </li>
+                            <li class="">
+                                <a href="faqs.php" title="">FAQs Page</a>
+                            </li>
+                            <li class="">
+                                <a href="404.php" title="">404 Page</a>
+                            </li>
+                            <li class="">
+                                <a href="password.php" title="">Coming Soon Page</a>
+                            </li>
+                            <li class="">
+                                <a href="cart.php" title="">Cart Page</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+            <li class="">
+                <a href="collections.php" title="">Collections</a>
+            </li>
+            <li class="">
+                <a href="blogs/news.php" title="">Blog</a>
+            </li>
+            <li class="">
+                <a href="pages/contact-us.php" title="">Contact</a>
+            </li>
+        </ul>
+    </div>
+</div>
+<div class="menuMobileOverlay hidden-md hidden-lg"></div>
+
+<div id="pageOverlay" class="page-overlay"></div>
+<div id="SearchModal" class="search-modal">
+        <div class="search-box-container">
+            <div class="close-bttn" onclick="closeSearchModal()">
+                <i class="fa fa-close"></i>
+            </div>
+
+            <div class="search-box">
+                <span>
+                    <input type="search" onsearch="searchProd()"  name="" id="search_query" placeholder="Search...">
+                </span>
+                <span class="search-icon" onclick="searchProd()" >
+                    <svg width="20px" height="20px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                        <g id="icomoon-ignore">
+                        </g>
+                        <path d="M28.591 27.273l-7.263-7.264c1.46-1.756 2.339-4.010 2.339-6.471 0-5.595-4.535-10.129-10.129-10.129-5.594 0-10.129 4.535-10.129 10.129 0 5.594 4.536 10.129 10.129 10.129 2.462 0 4.716-0.879 6.471-2.339l7.263 7.264 1.319-1.319zM4.475 13.538c0-4.997 4.065-9.063 9.063-9.063 4.997 0 9.063 4.066 9.063 9.063s-4.066 9.063-9.063 9.063c-4.998 0-9.063-4.066-9.063-9.063z" fill="#000000">
+                        
+                        </path>
+                    </svg>
+                </span>
+            </div>
+
+            <div class="search-shortcuts">
+                <span id="search_shortcuts" data-shortcut-value="Cushion Covers" onclick="shortcutSearch(this)">Cushion Covers</span>
+                <span id="search_shortcuts" data-shortcut-value="Bed Sheets" onclick="shortcutSearch(this)">Bed Sheets</span>
+                <span id="search_shortcuts" data-shortcut-value="Towels" onclick="shortcutSearch(this)">Towels</span>
+                <span id="search_shortcuts" data-shortcut-value="Bath Robe" onclick="shortcutSearch(this)">Bath Robe</span>
+                <span id="search_shortcuts" data-shortcut-value="Plain and texture" onclick="shortcutSearch(this)">Plain and texture</span>
+                <span id="search_shortcuts" data-shortcut-value="Curtains" onclick="shortcutSearch(this)">Curtains</span>
+                <span id="search_shortcuts" data-shortcut-value="Comforter" onclick="shortcutSearch(this)">Comforter</span>
+                <span id="search_shortcuts" data-shortcut-value="Rugs" onclick="shortcutSearch(this)">Rugs</span>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        document.onkeydown=function(){
+            if (window.event.keyCode >= 65 && window.event.keyCode <= 90) {
+                openSearchModal()
+                document.getElementById("search_query").focus();
+            }
+            if(window.event.keyCode === 27 ){
+                    closeSearchModal()
+            }
+        }
+        function openSearchModal(){
+            document.body.classList.add("modal-open")
+            document.getElementById("SearchModal").style.display = "flex"
+            document.getElementById("pageOverlay").style.display = "block"
+            // Get the current page scroll position
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        
+                // if any scroll is attempted, set this to the previous value
+                window.onscroll = function() {
+                    window.scrollTo(scrollLeft, scrollTop);
+                };
+        }
+        function closeSearchModal(){
+            document.getElementById("pageOverlay").style.display = "none"
+            document.getElementById("SearchModal").style.display = "none"
+
+
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            
+                // if any scroll is attempted, set this to the previous value
+                window.onscroll = function() {};
+        }
+        function shortcutSearch(query){
+            // alert(query.getAttribute("data-shortcut-value"))
+            let searchQuery = document.getElementById("search_query");
+            searchQuery.value = query.getAttribute("data-shortcut-value")
+            searchProd()
+
+        }
+        function searchProd(){
+            let searchQuery = document.getElementById("search_query").value;
+            if(searchQuery.length >= 1){
+                window.location.href = "/products?q="+searchQuery
+            }
+        }
+</script>
