4a02f0d1496422e3cd71256e7939d6fcd02dcfa6
Rishabh Nahar <rishabhnahar1202@gmail.com>
2023-02-27 19:25:04 +0530

    changes - email

--- cli_thri/api/mail_data.php
+++ cli_thri/api/mail_data.php
@@ -3,11 +3,9 @@
 
 session_start();
 date_default_timezone_set('Asia/Kolkata');
- 
 
-
-// if(isset($_POST['name'])){
-//     // Create a new PHPMailer instance
+if(isset($_FILES['resume'])){
+    // Create a new PHPMailer instance
     require ('./phpmailer/PHPMailerAutoload.php');
     $mail= new PHPMailer;
     // Set the SMTP settings
@@ -20,9 +18,9 @@ date_default_timezone_set('Asia/Kolkata');
     $mail->Port = 587;
 
     // Set the email details
-    $mail->setFrom("rishabh@gmail.com", "Your Name");
+    $mail->setFrom("rishabhpnahar@gmail.com", "Your Name");
     $mail->addAddress("rishabhnahar33@gmail.com", "Recipient Name");
-    $mail->Subject = "Resume Submission";
+    $mail->Subject = "Resume from ".$_POST["name"];
     $mail->isHTML(true);
 
     // Get the uploaded file details
@@ -34,7 +32,7 @@ date_default_timezone_set('Asia/Kolkata');
     // Check the file size
     $max_size = 10 * 1024 * 1024; // 10 MB
     if ($file_size > $max_size) {
-    header("Location: /?alert=File size is too large.");
+    header("Location: ../?alert=File size is too large.");
     die("Error: File size is too large.");
     }
 
@@ -43,7 +41,7 @@ date_default_timezone_set('Asia/Kolkata');
     $encoded_content = chunk_split(base64_encode($file_content));
 
     // Create the email message
-    $message = "<html><body><h2>Resume Submission</h2><p>Please find attached my resume.</p></body></html>";
+    $message = "<html><body><h2>Resume Submission:</h2><br><p>Name: ".$_POST['name']."</p><br><p>email: ".$_POST['email']."</p><br><p>Mobile: ".$_POST['mobile']."</p><br><p>Please find attached resume</p></body></html>";
     $mail->Body = $message;
     $mail->addStringAttachment($file_content, $file_name, "base64", $file_type);
 
@@ -51,12 +49,51 @@ date_default_timezone_set('Asia/Kolkata');
     if ($mail->send()) {
         echo "<script>alert('Email sent successfully.')</script>";
         // Redirect to the root URL
-        header("Location: /?alert=email sent successfully");
+        header("Location: ../?alert=Resume sent successfully");
         exit();
     } else {
-        header("Location: /?alert=Email sending failed");
+        header("Location: ../?alert=Email sending failed");
         echo "Email sending failed";
     }
-// }
+}
+
+if(isset($_POST['message'])){
+        // Create a new PHPMailer instance
+        require ('./phpmailer/PHPMailerAutoload.php');
+        $mail= new PHPMailer;
+        // Set the SMTP settings
+        $mail->isSMTP();
+        $mail->Host = "smtp.gmail.com";
+        $mail->SMTPAuth = true;
+        $mail->Username = "rishabhpnahar@gmail.com";
+        $mail->Password = "crigdqwxmxunttbz";
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
+    
+        // Set the email details
+        $mail->setFrom("rishabhpnahar@gmail.com", "Your Name");
+        $mail->addAddress("rishabhnahar33@gmail.com", "Recipient Name");
+        $mail->Subject = "Resume from ".$_POST["name"];
+        $mail->isHTML(true);
+
+        // Create the email message
+        $message = "<html><body>
+                        <h2>A message from ".$_POST["name"]." on AToZgroup.com</h2><br>
+                        <p>Email: ".$_POST['email']."</p>
+                        <p>Message: ".$_POST['message']."</p>
+                    </body></html>";
+        $mail->Body = $message;
+    
+        // Send the email
+        if ($mail->send()) {
+            echo "<script>alert('Email sent successfully.')</script>";
+            // Redirect to the root URL
+            header("Location: ../?alert=Message sent successfully");
+            exit();
+        } else {
+            header("Location: ../?alert=Email sending message");
+            echo "Email sending failed";
+        }
+}
 
 ?>
