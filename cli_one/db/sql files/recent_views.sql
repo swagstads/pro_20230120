b580154c38222b0735abea1411516603bc55c2fd
Rishabh Nahar <rishabhnahar1202@gmail.com>
2023-03-20 23:54:44 +0530

    recent views table

---
+++ cli_one/db/sql files/recent_views.sql
@@ -0,0 +1,86 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 20, 2023 at 07:23 PM
+-- Server version: 8.0.27
+-- PHP Version: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `atoz_ecom`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `recent_views`
+--
+
+CREATE TABLE `recent_views` (
+  `id` int NOT NULL,
+  `user_id` int UNSIGNED NOT NULL,
+  `product_id` int NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `recent_views`
+--
+
+INSERT INTO `recent_views` (`id`, `user_id`, `product_id`, `timestamp`) VALUES
+(17, 2, 9, '2023-03-20 16:43:34'),
+(18, 2, 10, '2023-03-20 16:43:43'),
+(19, 2, 6, '2023-03-20 16:43:46'),
+(20, 2, 8, '2023-03-20 16:44:10'),
+(21, 2, 5, '2023-03-20 16:43:56'),
+(22, 2, 7, '2023-03-20 16:44:46'),
+(23, 2, 4, '2023-03-20 16:59:28');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `recent_views`
+--
+ALTER TABLE `recent_views`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `recent_views`
+--
+ALTER TABLE `recent_views`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `recent_views`
+--
+ALTER TABLE `recent_views`
+  ADD CONSTRAINT `recent_views_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `recent_views_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
