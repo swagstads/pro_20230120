20b9a19ae83a7f5c7e926ad81d0391330313b33c
Rishabh Nahar <rishabhnahar1202@gmail.com>
2023-04-03 20:36:05 +0530

    footer newsletter UI

--- cli_one/footer.php
+++ cli_one/footer.php
@@ -172,6 +172,26 @@
                                 </ul>
                             </div>
                         </div>
+                        <div class="newsletter-subscription-form-wrapper">
+                            <h4>Subscribe</h4>
+                            <div class="newsletter-subscription-form-container">
+                                <span class="form-input">
+                                    <input type="text">
+                                </span>
+                                <span class="form-button"> 
+                                    
+                                    <button class="cssbuttons-io-button">
+                                    <div class="icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"></path></svg>
+                                    </div>
+                                    </button>
+
+                                </span>
+
+
+                            </div>
+                        </div>
+
 
                         <div class="velaFooterMenu">
                         <h4 class="velaFooterTitle" style="margin-top:30px">Payment Information</h4>
